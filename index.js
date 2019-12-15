2b9c07ef818e67f2fb6745a12de71e8d042a10cd
CodeCommando <sagartyagi.tyagi@gmail.com>
2019-12-15 16:32:48 +0530

    read the uploaded file

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const RPG = require('./rpg/RPG');
 
 const bodyParser = require('body-parser');
-const express = require('express');
 const formidable = require('formidable');
+const readline = require('readline');
+const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 9123;
@@ -16,7 +17,7 @@ app.use('/', express.static('public'));
 app.post('/convert', function(req, res) {
   var lines = req.body.lines;
   var indent = req.body.indent;
- 
+ // console.log(' lines in  :'  + lines ); 
   lines.push('', '');
  
   var conv = new RPG(lines, Number(indent));
@@ -32,11 +33,31 @@ app.post('/fileupload', function(req, res) {
       form.parse(req, function (err, fields, files) {
         var oldpath = files.filetoupload.path;
         var newpath = 'C:/Users/krishna/Documents/a/' + files.filetoupload.name;
+        var newLine = ' ' ; 
         fs.rename(oldpath, newpath, function (err) {
           if (err) throw err;
-          var contents = fs.readFileSync(newpath, 'utf8');
-          console.log('contents : ' + contents);
-          //res.write('File uploaded and moved!');
+            const readInterface = readline.createInterface({
+              input: fs.createReadStream(newpath),
+             // output: process.stdout,
+              console: false
+            });  
+           
+            let i = 0 ;  
+            readInterface.on('line', function(line) {
+            //console.log('line  '+ i++ + ' :' + line);
+            newLine = newLine + line + ' ,' ; 
+            //console.log(' new line  '+ newLine);
+         
+             });
+
+            // end
+            readInterface.on('close', function(line) {
+              var conv = new RPG(newLine, Number('2'));
+              conv.parse();
+              let c = JSON.stringify(conv.lines); 
+              console.log(' something: ' + c);
+            }); 
+                      
           res.end();
         });
    });
