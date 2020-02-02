b39b5cc33bc74347aeeddac90b020faee7550282
CodeCommando <sagartyagi.tyagi@gmail.com>
2020-02-03 01:22:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 9123;
+const port = 9124;
 
 app.use(bodyParser.json({limit: '10mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true }));
